e25cb40f75c32592b888fd23f90a3de542051084
AhmadAbdAlRhman <112072240+AhmadAbdAlRhman@users.noreply.github.com>
2024-10-05 17:54:16 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 // import logo from './logo.svg';
 import p6 from "./photo_2024-10-05_17-11-25.jpg"
 import './App.css';
-
 function App() {
   return (
     <div className="card">
       <div className="C1">
         <img className="imageDashboard" src={p6} alt="helooo i am not heer" />
       </div>
-
       <div className="C2">
         <div className="productdetails">
           <h1 className="productName">
@@ -21,14 +19,11 @@ function App() {
             <img className="pdf" src={p6} alt="helooo i am not heer" />
           </div>
         </div>
-
         <p className="C3"> <a href="#">Contact Us </a>
           to order & inquire about this product 
         </p>
       </div>
-     
     </div>
   );
 }
-
 export default App;
