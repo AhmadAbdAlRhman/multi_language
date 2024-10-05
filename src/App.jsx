d9b68219f308323c1b1c7692ab9b940fbcff2525
AhmadAbdAlRhman <112072240+AhmadAbdAlRhman@users.noreply.github.com>
2024-10-05 17:50:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import p6 from "./photo_2024-10-05_17-11-25.jpg"
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+      <div className="C1">
+        <img className="imageDashboard" src={p6} alt="helooo i am not heer" />
+      </div>
+
+      <div className="C2">
+        <div className="productdetails">
+          <h1 className="productName">
+            Lift Check Valve with SS screen DN350-1000{" "}
+          </h1>
+          <div className="PI">
+            <p className="productdeDescription">
+              hghgfhg mnzdbj,hvglwf ,hlkhlkaf ,kamsbdlhjhblascm,hhhhhhhhhhhhhbjjhsjhb,,,,,,,,,,,,,,,,,,,,,cmsncljhaljfm,n,jbljgasfcb,,,,cmbkugougefhnl
+            </p>
+            <img className="pdf" src={p6} alt="helooo i am not heer" />
+          </div>
+        </div>
+
+        <p className="C3"> <a href="#">Contact Us </a>
+          to order & inquire about this product 
+        </p>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
